0613d76eea8bc52f40445189f9bde352dd4e50ce
Adizmobile <adizmobile.com@gmail.com>
2018-05-16 10:56:02 +0700

    java For

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int tong = 0;
+        for (int i = 0; i< 10; i++){
+            tong = tong + i;
+        }
+        System.out.println("Tổng Từ 0 Đến 9 Sử Dụng Vòng Lặp For Là :" + tong);
+    }
+}
